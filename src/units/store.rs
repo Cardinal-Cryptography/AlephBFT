77673bd0bce3bc4d5c7525a45a8eab1f383d26fa
Jedrzej Kula <jedrzej.kula@cardinals.cc>
2021-08-06 03:08:40 +0200

    fix after review

--- src/units/store.rs
+++ src/units/store.rs
@@ -10,7 +10,6 @@ pub(crate) struct UnitStore<'a, H: Hasher, D: Data, KB: KeyBox> {
     by_hash: HashMap<H::Hash, SignedUnit<'a, H, D, KB>>,
     parents: HashMap<H::Hash, Vec<H::Hash>>,
     //the number of unique nodes that we hold units for a given round
-    n_units_per_round: Vec<NodeCount>,
     is_forker: NodeMap<bool>,
     legit_buffer: Vec<SignedUnit<'a, H, D, KB>>,
     max_round: Round,
@@ -22,7 +21,6 @@ impl<'a, H: Hasher, D: Data, KB: KeyBox> UnitStore<'a, H, D, KB> {
             by_coord: HashMap::new(),
             by_hash: HashMap::new(),
             parents: HashMap::new(),
-            n_units_per_round: vec![NodeCount(0); (max_round + 1).into()],
             // is_forker is initialized with default values for bool, i.e., false
             is_forker: NodeMap::new_with_len(n_nodes),
             legit_buffer: Vec::new(),
@@ -63,8 +61,8 @@ impl<'a, H: Hasher, D: Data, KB: KeyBox> UnitStore<'a, H, D, KB> {
         self.is_forker[node_id]
     }
 
-    // Marks a node as a forker and outputs units in store of round <= round_in_progress created by this node.
-    // The returned vector is sorted w.r.t. increasing rounds. Units of higher round created by this node are removed from store.
+    // Marks a node as a forker and outputs all units in store created by this node.
+    // The returned vector is sorted w.r.t. increasing rounds.
     pub(crate) fn mark_forker(&mut self, forker: NodeIndex) -> Vec<SignedUnit<'a, H, D, KB>> {
         if self.is_forker[forker] {
             warn!(target: "AlephBFT-unit-store", "Trying to mark the node {:?} as forker for the second time.", forker);
@@ -77,7 +75,6 @@ impl<'a, H: Hasher, D: Data, KB: KeyBox> UnitStore<'a, H, D, KB> {
 
     pub(crate) fn add_unit(&mut self, su: SignedUnit<'a, H, D, KB>, alert: bool) {
         let hash = su.as_signable().hash();
-        let round = su.as_signable().round();
         let creator = su.as_signable().creator();
 
         if alert {
@@ -93,17 +90,8 @@ impl<'a, H: Hasher, D: Data, KB: KeyBox> UnitStore<'a, H, D, KB> {
             return;
         }
         self.by_hash.insert(hash, su.clone());
-        let coord = su.as_signable().coord();
-        // We do not store multiple forks of a unit by coord, as there is never a need to
-        // fetch all units corresponding to a particular coord.
-        if self.by_coord.insert(coord, su.clone()).is_none() {
-            // This means that this unit is not a fork (even though the creator might be a forker)
-            self.n_units_per_round[round as usize] += NodeCount(1);
-        }
-        // NOTE: a minor inefficiency is that we send alerted units of high rounds that are possibly
-        // way beyond round_in_progress right away to Consensus. This could be perhaps corrected so that
-        // we wait until the round is in progress, but this does not seem to help vs actual attacks and in
-        // "accidental" forks the rounds will never be much higher than round_in_progress.
+        self.by_coord.insert(su.as_signable().coord(), su.clone());
+
         if alert || !self.is_forker[creator] {
             self.legit_buffer.push(su);
         }
@@ -150,40 +138,6 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn add_unit_increment_count_test() {
-        let n_nodes = NodeCount(10);
-
-        let mut store = UnitStore::<Hasher64, Data, KeyBox>::new(n_nodes, 100);
-
-        let keybox = KeyBox::new(n_nodes, NodeIndex(0));
-        let unit = create_unit(0, NodeIndex(0), n_nodes, 0, &keybox).await;
-
-        assert_eq!(NodeCount(0), store.n_units_per_round[0]);
-        store.add_unit(unit, false);
-        assert_eq!(NodeCount(1), store.n_units_per_round[0]);
-    }
-
-    #[tokio::test]
-    async fn add_units_increment_round_test() {
-        let n_nodes = NodeCount(10);
-
-        let mut store = UnitStore::<Hasher64, Data, KeyBox>::new(n_nodes, 100);
-
-        let keyboxes: Vec<_> = (0..=4)
-            .map(|i| KeyBox::new(n_nodes, NodeIndex(i)))
-            .collect();
-
-        assert_eq!(NodeCount(0), store.n_units_per_round[0]);
-
-        for (i, keybox) in keyboxes.iter().enumerate() {
-            let unit = create_unit(0, NodeIndex(i), n_nodes, 0, keybox).await;
-            store.add_unit(unit, false);
-        }
-
-        assert_eq!(NodeCount(5), store.n_units_per_round[0]);
-    }
-
-    #[tokio::test]
     async fn mark_forker_restore_state() {
         let n_nodes = NodeCount(10);
 
@@ -225,12 +179,10 @@ mod tests {
         for (round, hash) in forker_hashes[0..4].iter().enumerate() {
             let round = round as Round;
             let coord = UnitCoord::new(round, NodeIndex(0));
-            assert_eq!(NodeCount(5), store.n_units_per_round[round as usize]);
             assert!(store.by_coord.contains_key(&coord));
             assert!(store.by_hash.contains_key(hash));
         }
 
-        assert_eq!(NodeCount(1), store.n_units_per_round[4]);
         assert!(store
             .by_coord
             .contains_key(&UnitCoord::new(4, NodeIndex(0))));
@@ -240,7 +192,6 @@ mod tests {
             let round = round as Round;
             let round = round + 5;
             let coord = UnitCoord::new(round, NodeIndex(0));
-            assert_eq!(NodeCount(1), store.n_units_per_round[round as usize]);
             assert!(store.by_coord.contains_key(&coord));
             assert!(store.by_hash.contains_key(hash));
         }
