7eb77f86b10264d929925c6da98574f6bdaffc08
timorl <tymorl@gmail.com>
2021-10-19 16:09:52 +0200

    Auto publish to crates.io on manual version bumps
    Also improve local pipeline somewhat and add Cargo.locks to version
    control.

--- run_local_pipeline.sh
+++ run_local_pipeline.sh
@@ -5,3 +5,18 @@ set -e
 cargo +nightly clippy --all-targets --all-features -- -D warnings -A clippy::type_complexity
 cargo +nightly fmt --all
 cargo test --lib -- --skip medium
+
+pushd fuzz
+
+cargo +nightly clippy --all-targets --all-features -- -D warnings -A clippy::type_complexity
+cargo +nightly fmt --all
+cargo test --lib -- --skip medium
+
+popd
+pushd mock
+
+cargo +nightly clippy --all-targets --all-features -- -D warnings -A clippy::type_complexity
+cargo +nightly fmt --all
+cargo test --lib -- --skip medium
+
+popd
