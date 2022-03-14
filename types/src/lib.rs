609acc0f39958383f06b6512a6758a7e7acd82f6
Damian Leśniak <damian.lesniak@cardinals.cc>
2022-03-14 20:52:33 +0100

    1

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,34 +1,14 @@
-//! Implements the Aleph BFT Consensus protocol as a "finality gadget". The [Member] struct
-//! requires access to a network layer, a cryptographic primitive, and a data provider that
-//! gives appropriate access to the set of available data that we need to make consensus on.
 use async_trait::async_trait;
 use codec::{Decode, Encode};
-use futures::{channel::mpsc, Future};
-use std::{fmt::Debug, hash::Hash as StdHash, pin::Pin};
+use std::{fmt::Debug, hash::Hash as StdHash};
 
-use crate::nodes::NodeMap;
-
-pub use config::{default_config, exponential_slowdown, Config, DelayConfig};
-pub use member::run_session;
-pub use network::{Network, NetworkData, Recipient};
-pub use nodes::{NodeCount, NodeIndex};
-
-mod alerts;
-mod consensus;
-mod creation;
-mod extender;
-mod member;
-mod network;
 mod nodes;
-mod runway;
-mod signed;
-pub use signed::*;
-mod config;
-pub mod rmc;
-mod terminal;
-#[cfg(test)]
-pub mod testing;
-mod units;
+pub use nodes::{
+    NodeCount,
+    NodeIndex,
+    NodeMap,
+    NodeSubset,
+};
 
 /// The number of a session for which the consensus is run.
 pub type SessionId = u64;
@@ -84,27 +64,3 @@ pub trait Data: Eq + Clone + Send + Sync + Debug + StdHash + Encode + Decode + '
 
 impl<T> Data for T where T: Eq + Clone + Send + Sync + Debug + StdHash + Encode + Decode + 'static {}
 
-/// An asynchronous round of the protocol.
-pub type Round = u16;
-
-/// Type for sending a new ordered batch of data items.
-pub type OrderedBatch<Data> = Vec<Data>;
-
-/// A handle for waiting the task's completion.
-pub type TaskHandle = Pin<Box<dyn Future<Output = Result<(), ()>> + Send>>;
-
-/// An abstraction for an execution engine for Rust's asynchronous tasks.
-pub trait SpawnHandle: Clone + Send + 'static {
-    /// Run a new task.
-    fn spawn(&self, name: &'static str, task: impl Future<Output = ()> + Send + 'static);
-    /// Run a new task and returns a handle to it. If there is some error or panic during
-    /// execution of the task, the handle should return an error.
-    fn spawn_essential(
-        &self,
-        name: &'static str,
-        task: impl Future<Output = ()> + Send + 'static,
-    ) -> TaskHandle;
-}
-
-pub(crate) type Receiver<T> = mpsc::UnboundedReceiver<T>;
-pub(crate) type Sender<T> = mpsc::UnboundedSender<T>;
