76d932d0f51c1cc28d1217f00f4b02e3b5e9651c
Damian Leśniak <damian.lesniak@cardinals.cc>
2022-03-22 16:18:55 +0100

    fixing tests - part 2: removing UncheckedSigned::as_signable_mut

--- consensus/src/testing/mock.rs
+++ consensus/src/testing/mock.rs
@@ -295,7 +295,7 @@ impl UnreliableRouter {
         }
     }
 
-    pub fn add_hook<HK: NetworkHook + 'static>(&mut self, hook: HK) {
+    pub(crate) fn add_hook<HK: NetworkHook + 'static>(&mut self, hook: HK) {
         self.hook_list.borrow_mut().push(Box::new(hook));
     }
 
@@ -359,7 +359,14 @@ impl Future for UnreliableRouter {
 
             if let Some(peer) = this.peers.borrow().get(&recipient) {
                 for hook in this.hook_list.borrow_mut().iter_mut() {
-                    hook.update_state(&mut data, sender, recipient);
+                    match hook
+                        .update_state(&mut data, sender, recipient)
+                        .as_mut()
+                        .poll(cx)
+                    {
+                        Poll::Ready(()) => (),
+                        Poll::Pending => panic!(),
+                    }
                 }
                 peer.tx
                     .unbounded_send((data, sender))
@@ -374,8 +381,15 @@ impl Future for UnreliableRouter {
     }
 }
 
-pub trait NetworkHook: Send {
-    fn update_state(&mut self, data: &mut NetworkData, sender: NodeIndex, recipient: NodeIndex);
+#[async_trait]
+pub(crate) trait NetworkHook: Send {
+    /// This must complete during a single poll
+    async fn update_state(
+        &mut self,
+        data: &mut NetworkData,
+        sender: NodeIndex,
+        recipient: NodeIndex,
+    );
 }
 
 #[derive(Clone)]
@@ -402,8 +416,14 @@ impl AlertHook {
     }
 }
 
+#[async_trait]
 impl NetworkHook for AlertHook {
-    fn update_state(&mut self, data: &mut NetworkData, sender: NodeIndex, recipient: NodeIndex) {
+    async fn update_state(
+        &mut self,
+        data: &mut NetworkData,
+        sender: NodeIndex,
+        recipient: NodeIndex,
+    ) {
         use crate::{alerts::AlertMessage::*, network::NetworkDataInner::*};
         if let crate::NetworkData(Alert(ForkAlert(_))) = data {
             *self
--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -39,17 +39,10 @@ impl<T: Signable, S: Signature> UncheckedSigned<Indexed<T>, S> {
 // this should be removed!
 // #[cfg(test)]
 impl<T: Signable, S: Signature> UncheckedSigned<T, S> {
-    pub fn as_signable_mut(&mut self) -> &mut T {
-        &mut self.signable
-    }
     pub fn signature_mut(&mut self) -> &mut S {
         &mut self.signature
     }
 }
-
-// #[cfg(test)]
-// impl<T: Signable, S: Signature> UncheckedSigned<T, S> {
-// }
 
 /// Error type returned when a verification of a signature fails.
 #[derive(Clone, Debug)]
