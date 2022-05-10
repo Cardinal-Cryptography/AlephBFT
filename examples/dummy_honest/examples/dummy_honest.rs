3135c984744669aeb867c73b7796dfd6ec820850
Damian Leśniak <damian.lesniak@cardinals.cc>
2022-05-10 22:43:25 +0200

    dummy honest without libp2p

--- examples/dummy_honest/examples/dummy_honest.rs
+++ examples/dummy_honest/examples/dummy_honest.rs
@@ -1,28 +1,12 @@
-use aleph_bft::{run_session, Recipient};
-use aleph_bft_mock::{
-    Data, DataProvider, FinalizationHandler, Hasher64, Keychain, Loader, PartialMultisignature,
-    Saver, Signature, Spawner,
-};
+use aleph_bft::run_session;
+use aleph_bft_examples_network::Network;
+use aleph_bft_mock::{DataProvider, FinalizationHandler, Keychain, Loader, Saver, Spawner};
 use chrono::Local;
 use clap::Parser;
-use codec::{Decode, Encode};
-use futures::{
-    channel::{mpsc, oneshot},
-    FutureExt, StreamExt,
-};
-use libp2p::{
-    development_transport,
-    floodsub::{self, Floodsub, FloodsubEvent},
-    identity,
-    mdns::{Mdns, MdnsConfig, MdnsEvent},
-    swarm::{NetworkBehaviourEventProcess, SwarmBuilder},
-    NetworkBehaviour, PeerId, Swarm,
-};
-use log::{debug, error, info, warn};
+use futures::{channel::oneshot, StreamExt};
+use log::{debug, error, info};
 use parking_lot::Mutex;
-use std::{error::Error, io::Write, sync::Arc, time::Duration};
-
-const ALEPH_PROTOCOL_NAME: &str = "aleph";
+use std::{io::Write, sync::Arc};
 
 /// Dummy honest node example.
 #[derive(Parser, Debug)]
@@ -32,9 +16,9 @@ struct Args {
     #[clap(long)]
     my_id: usize,
 
-    /// Size of the committee
-    #[clap(long)]
-    n_members: usize,
+    /// Ports
+    #[clap(long, value_delimiter = ',')]
+    ports: Vec<usize>,
 
     /// Number of data to be finalized
     #[clap(long)]
@@ -59,13 +43,10 @@ async fn main() {
     let args = Args::parse();
 
     info!(target: "dummy-honest", "Getting network up.");
-    let (network, mut manager) = Network::new().await.unwrap();
-    let (close_network, exit) = oneshot::channel();
-    let network_handle = tokio::spawn(async move { manager.run(exit).await });
-
+    let network = Network::new(args.my_id, &args.ports).await.unwrap();
+    let n_members = args.ports.len();
     let data_provider = DataProvider::new();
     let (finalization_handler, mut finalized_rx) = FinalizationHandler::new();
-
     let backup_loader = Loader::new(vec![]);
     let backup_saver = Saver::new(Arc::new(Mutex::new(vec![])));
     let local_io = aleph_bft::LocalIO::new(
@@ -77,8 +58,8 @@ async fn main() {
 
     let (close_member, exit) = oneshot::channel();
     let member_handle = tokio::spawn(async move {
-        let keychain = Keychain::new(args.n_members.into(), args.my_id.into());
-        let config = aleph_bft::default_config(args.n_members.into(), args.my_id.into(), 0);
+        let keychain = Keychain::new(n_members.into(), args.my_id.into());
+        let config = aleph_bft::default_config(n_members.into(), args.my_id.into(), 0);
         run_session(config, local_io, network, keychain, Spawner {}, exit).await
     });
 
@@ -93,131 +74,4 @@ async fn main() {
     }
     close_member.send(()).expect("should send");
     member_handle.await.unwrap();
-    close_network.send(()).expect("should send");
-    network_handle.await.unwrap();
-}
-
-#[derive(NetworkBehaviour)]
-struct Behaviour {
-    mdns: Mdns,
-    floodsub: Floodsub,
-    #[behaviour(ignore)]
-    peers: Vec<PeerId>,
-    #[behaviour(ignore)]
-    msg_tx: mpsc::UnboundedSender<Vec<u8>>,
-}
-
-impl NetworkBehaviourEventProcess<MdnsEvent> for Behaviour {
-    fn inject_event(&mut self, event: MdnsEvent) {
-        if let MdnsEvent::Discovered(list) = event {
-            for (peer, _) in list {
-                if self.peers.iter().any(|p| *p == peer) {
-                    continue;
-                }
-                self.peers.push(peer);
-                self.floodsub.add_node_to_partial_view(peer);
-            }
-        }
-    }
-}
-
-impl NetworkBehaviourEventProcess<FloodsubEvent> for Behaviour {
-    fn inject_event(&mut self, message: FloodsubEvent) {
-        if let FloodsubEvent::Message(message) = message {
-            self.msg_tx
-                .unbounded_send(message.data)
-                .expect("should succeed");
-        }
-    }
-}
-
-struct NetworkManager {
-    swarm: Swarm<Behaviour>,
-    outgoing_rx: mpsc::UnboundedReceiver<Vec<u8>>,
-}
-
-impl NetworkManager {
-    async fn run(&mut self, exit: oneshot::Receiver<()>) {
-        let mut exit = exit.into_stream();
-        loop {
-            tokio::select! {
-                maybe_pm = self.outgoing_rx.next() => {
-                    if let Some(message) = maybe_pm {
-                        let floodsub = &mut self.swarm.behaviour_mut().floodsub;
-                        let topic = floodsub::Topic::new(ALEPH_PROTOCOL_NAME);
-                        floodsub.publish(topic, message);
-                    }
-                }
-                event = self.swarm.next() => {
-                    event.unwrap();
-                }
-                _ = exit.next() => break,
-            }
-        }
-    }
-}
-
-type NetworkData = aleph_bft::NetworkData<Hasher64, Data, Signature, PartialMultisignature>;
-
-struct Network {
-    outgoing_tx: mpsc::UnboundedSender<Vec<u8>>,
-    msg_rx: mpsc::UnboundedReceiver<Vec<u8>>,
-}
-
-#[async_trait::async_trait]
-impl aleph_bft::Network<NetworkData> for Network {
-    fn send(&self, data: NetworkData, _recipient: Recipient) {
-        if let Err(e) = self.outgoing_tx.unbounded_send(data.encode()) {
-            warn!(target: "dummy-honest", "Failed network send: {:?}", e)
-        }
-    }
-    async fn next_event(&mut self) -> Option<NetworkData> {
-        self.msg_rx.next().await.map(|msg| {
-            NetworkData::decode(&mut &msg[..]).expect("honest network data should decode")
-        })
-    }
-}
-
-impl Network {
-    async fn new() -> Result<(Self, NetworkManager), Box<dyn Error>> {
-        let local_key = identity::Keypair::generate_ed25519();
-        let local_peer_id = PeerId::from(local_key.public());
-        info!(target: "dummy-honest", "Local peer id: {:?}", local_peer_id);
-
-        let transport = development_transport(local_key).await?;
-
-        let topic = floodsub::Topic::new(ALEPH_PROTOCOL_NAME);
-
-        let (msg_tx, msg_rx) = mpsc::unbounded();
-        let mut swarm = {
-            let mdns_config = MdnsConfig {
-                ttl: Duration::from_secs(6 * 60),
-                query_interval: Duration::from_millis(100),
-            };
-            let mdns = Mdns::new(mdns_config).await?;
-            let mut behaviour = Behaviour {
-                floodsub: Floodsub::new(local_peer_id),
-                mdns,
-                peers: vec![],
-                msg_tx: msg_tx.clone(),
-            };
-            behaviour.floodsub.subscribe(topic.clone());
-            SwarmBuilder::new(transport, behaviour, local_peer_id)
-                .executor(Box::new(|fut| {
-                    tokio::spawn(fut);
-                }))
-                .build()
-        };
-
-        swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-
-        let (outgoing_tx, outgoing_rx) = mpsc::unbounded();
-        let network = Network {
-            outgoing_tx,
-            msg_rx,
-        };
-        let network_manager = NetworkManager { swarm, outgoing_rx };
-
-        Ok((network, network_manager))
-    }
 }
---
+++ examples/network/src/lib.rs
@@ -0,0 +1,79 @@
+use aleph_bft::Recipient;
+use aleph_bft_mock::{Data, Hasher64, PartialMultisignature, Signature};
+use codec::{Decode, Encode};
+use log::error;
+use std::{io::Write, net::SocketAddr};
+use tokio::{
+    io::{self, AsyncReadExt},
+    net::TcpListener,
+};
+
+pub type NetworkData = aleph_bft::NetworkData<Hasher64, Data, Signature, PartialMultisignature>;
+
+pub struct Network {
+    my_id: usize,
+    addresses: Vec<SocketAddr>,
+    listener: TcpListener,
+}
+
+impl Network {
+    pub async fn new(my_id: usize, ports: &[usize]) -> Result<Self, Box<dyn std::error::Error>> {
+        assert!(my_id < ports.len());
+        let mut addresses = vec![];
+        for port in ports {
+            addresses.push(format!("127.0.0.1:{}", port).parse::<SocketAddr>()?);
+        }
+        let listener =
+            TcpListener::bind(format!("127.0.0.1:{}", ports.get(my_id).unwrap())).await?;
+        Ok(Network {
+            my_id,
+            addresses,
+            listener,
+        })
+    }
+    fn send2(&self, data: NetworkData, recipient: &Recipient) -> io::Result<()> {
+        match recipient {
+            Recipient::Everyone => {
+                for r in 0..self.addresses.len() {
+                    self.send_to_peer(data.clone(), r)?;
+                }
+            }
+            Recipient::Node(r) => self.send_to_peer(data, (*r).0)?,
+        }
+        Ok(())
+    }
+    fn send_to_peer(&self, data: NetworkData, recipient: usize) -> io::Result<()> {
+        assert!(recipient < self.addresses.len());
+        if recipient != self.my_id {
+            let mut stream = std::net::TcpStream::connect(self.addresses.get(recipient).unwrap())?;
+            stream.write_all(&data.encode())?;
+        }
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl aleph_bft::Network<NetworkData> for Network {
+    fn send(&self, data: NetworkData, recipient: Recipient) {
+        match self.send2(data, &recipient) {
+            Ok(_) => (),
+            Err(_) => error!("Sending failed, recipient: {:?}", recipient),
+        };
+    }
+    async fn next_event(&mut self) -> Option<NetworkData> {
+        let mut buffer = Vec::new();
+        match self.listener.accept().await {
+            Ok((mut socket, _addr)) => match socket.read_to_end(&mut buffer).await {
+                Ok(_) => NetworkData::decode(&mut &buffer[..]).ok(),
+                Err(_) => {
+                    error!("Could not decode incoming data");
+                    None
+                }
+            },
+            Err(e) => {
+                error!("Couldn't accept connection: {:?}", e);
+                None
+            }
+        }
+    }
+}
