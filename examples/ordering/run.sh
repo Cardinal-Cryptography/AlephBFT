1ab7e900dccfddbb882fb982bcd93ea4f2b4478b
Damian Leśniak <damian.lesniak@cardinals.cc>
2022-05-30 16:33:44 +0200

    upgrade bash scripts

--- examples/ordering/run.sh
+++ examples/ordering/run.sh
@@ -9,3 +9,7 @@ clear
 for i in {0..4}; do
     cargo run --release -- --id "$i" --ports 43000,43001,43002,43003,43004 --n-items 50 2> "node$i.log" &
 done
+
+echo "Running ordering example... (Ctrl+C to exit)"
+trap 'kill $(jobs -p)' SIGINT SIGTERM
+wait $(jobs -p)
