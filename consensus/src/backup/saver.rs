d71c8f445f2b0f49c765fd83250a3c486c258bed
Lukasz Gniecki <lukasgniecki@gmail.com>
2023-08-31 21:02:22 +0200

    unit test for saver

--- consensus/src/backup/saver.rs
+++ consensus/src/backup/saver.rs
@@ -1,14 +1,13 @@
-use crate::{
-    units::UncheckedSignedUnit, Data, Hasher, MultiKeychain, Receiver, Sender, Terminator,
-};
+use std::io::Write;
 
-use crate::alerts::AlertData;
 use codec::Encode;
 use futures::{FutureExt, StreamExt};
-
-use crate::backup::BackupItem;
 use log::{debug, error};
-use std::io::Write;
+
+use crate::{
+    alerts::AlertData, backup::BackupItem, units::UncheckedSignedUnit, Data, Hasher, MultiKeychain,
+    Receiver, Sender, Terminator,
+};
 
 const LOG_TARGET: &str = "AlephBFT-backup-saver";
 
@@ -98,5 +97,135 @@ impl<H: Hasher, D: Data, MK: MultiKeychain, W: Write> BackupSaver<H, D, MK, W> {
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::{
+        channel::{mpsc, oneshot},
+        StreamExt,
+    };
+
+    use aleph_bft_mock::{Data, Hasher64, Keychain, Saver, Signature};
+
+    use crate::{
+        alerts::{Alert, AlertData},
+        backup::BackupSaver,
+        units::{creator_set, preunit_to_unchecked_signed_unit, UncheckedSignedUnit},
+        NodeCount, NodeIndex, Terminator,
+    };
+
+    type TestBackupSaver = BackupSaver<Hasher64, Data, Keychain, Saver>;
+    type TestUnit = UncheckedSignedUnit<Hasher64, Data, Signature>;
+    type TestAlertData = AlertData<Hasher64, Data, Keychain>;
+    struct PrepareSaverResponse<F: futures::Future> {
+        task: F,
+        units_for_saver: mpsc::UnboundedSender<TestUnit>,
+        units_from_saver: mpsc::UnboundedReceiver<TestUnit>,
+        alerts_for_saver: mpsc::UnboundedSender<TestAlertData>,
+        alerts_from_saver: mpsc::UnboundedReceiver<TestAlertData>,
+        exit_tx: oneshot::Sender<()>,
+    }
+
+    fn prepare_saver() -> PrepareSaverResponse<impl futures::Future> {
+        let (units_for_saver, units_from_runway) = mpsc::unbounded();
+        let (units_for_runway, units_from_saver) = mpsc::unbounded();
+        let (alerts_for_saver, alerts_from_alerter) = mpsc::unbounded();
+        let (alerts_for_alerter, alerts_from_saver) = mpsc::unbounded();
+        let (exit_tx, exit_rx) = oneshot::channel();
+        let backup = Saver::new();
+
+        let task = {
+            let mut saver: TestBackupSaver = BackupSaver::new(
+                units_from_runway,
+                alerts_from_alerter,
+                units_for_runway,
+                alerts_for_alerter,
+                backup,
+            );
+
+            async move {
+                saver.run(Terminator::create_root(exit_rx, "saver")).await;
+            }
+        };
+
+        PrepareSaverResponse {
+            task,
+            units_for_saver,
+            units_from_saver,
+            alerts_for_saver,
+            alerts_from_saver,
+            exit_tx,
+        }
+    }
+
+    #[tokio::test]
+    async fn test_proper_response_ordering() {
+        let PrepareSaverResponse {
+            task,
+            units_for_saver,
+            mut units_from_saver,
+            alerts_for_saver,
+            mut alerts_from_saver,
+            exit_tx,
+        } = prepare_saver();
+
+        let handle = tokio::spawn(async {
+            task.await;
+        });
+
+        let creators = creator_set(NodeCount(5));
+        let keychains: Vec<_> = (0..5)
+            .map(|id| Keychain::new(NodeCount(5), NodeIndex(id)))
+            .collect();
+        let units: Vec<TestUnit> = (0..5)
+            .map(|k| {
+                preunit_to_unchecked_signed_unit(
+                    creators[k].create_unit(0).unwrap().0,
+                    0,
+                    &keychains[k],
+                )
+            })
+            .collect();
+        let alerts: Vec<TestAlertData> = (0..5)
+            .map(|k| {
+                TestAlertData::OwnAlert(Alert::new(
+                    NodeIndex(0),
+                    (units[k].clone(), units[k].clone()),
+                    vec![],
+                ))
+            })
+            .collect();
+
+        let backup_save_ordering = vec![0, 1, 1, 1, 0, 0, 1, 0, 1, 0];
+        let mut ui = 0;
+        let mut ai = 0;
+        for i in backup_save_ordering {
+            if i == 0 {
+                units_for_saver
+                    .unbounded_send(units.get(ui).unwrap().clone())
+                    .unwrap();
+                ui += 1;
+            } else {
+                alerts_for_saver
+                    .unbounded_send(alerts.get(ai).unwrap().clone())
+                    .unwrap();
+                ai += 1;
+            }
+        }
+
+        for u in units {
+            let u_backup = units_from_saver.next().await.unwrap();
+            assert_eq!(u, u_backup);
+        }
+
+        for a in alerts {
+            let a_backup = alerts_from_saver.next().await.unwrap();
+            assert_eq!(a, a_backup);
+        }
+
+        exit_tx.send(()).unwrap();
+        handle.await.unwrap();
     }
 }
