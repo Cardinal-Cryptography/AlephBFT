73b4233bc70a54d80e28880ef816f21ecd5cbf17
Marcin <Marcin.Radecki@cardinals.cc>
2024-12-12 08:43:40 +0100

    Revered changes to unit testing code

--- consensus/src/units/testing.rs
+++ consensus/src/units/testing.rs
@@ -241,7 +241,7 @@ pub fn minimal_reconstructed_dag_units_up_to(
         .clone();
     let inactive_node = inactive_node_first_and_last_seen_unit.creator();
     for r in 1..=round {
-        let direct_parents: Vec<TestingDagUnit> = dag
+        let mut parents: Vec<TestingDagUnit> = dag
             .last()
             .expect("previous round present")
             .clone()
@@ -250,7 +250,6 @@ pub fn minimal_reconstructed_dag_units_up_to(
             .choose_multiple(&mut rng, threshold)
             .into_iter()
             .collect();
-        let mut parents = direct_parents.clone();
         if r == round {
             let ancestor_unit = dag
                 .first()
